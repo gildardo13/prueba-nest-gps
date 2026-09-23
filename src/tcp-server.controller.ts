3ea597b69c640acdc7fa90eb8ea901f39b9bb68c
CICSACORP\grubalcaba <grubalcaba@cicsagruas.net>
2026-09-22 20:57:31 -0600

    fix/eror

--- src/tcp-server.controller.ts
+++ src/tcp-server.controller.ts
@@ -1,10 +1,11 @@
 
 
 import { Body, Controller, Post } from '@nestjs/common';
+import { GpsService } from './tcp-server.service';
 
 @Controller('gps')
 export class GpsController {
-    constructor(private readonly gpsService:any) { }
+    constructor(private readonly gpsService:GpsService) { }
 
     @Post('/telemetry')
     receiveTelemetry(@Body() body: any) {
