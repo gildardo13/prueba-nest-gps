3d822440412e342d264178d3dae78ef043b5757e
CICSACORP\grubalcaba <grubalcaba@cicsagruas.net>
2026-09-22 20:43:30 -0600

    ifx

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GpsController } from './tcp-server.controller';
+import { GpsService } from './tcp-server.service';
 
 @Module({
   imports: [],
   controllers: [AppController,
     GpsController
   ],
-  providers: [AppService],
+  providers: [AppService, GpsService],
 })
 export class AppModule {}
